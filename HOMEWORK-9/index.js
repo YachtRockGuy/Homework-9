3aba76b9fab7df6402bdbc54b467e6fc0bb1a77a
Chris Havel <christopher.havel@me.com>
2021-01-27 19:42:18 -0500

    first commit

---
+++ HOMEWORK-9/index.js
@@ -0,0 +1,61 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ]);
+};
+
+const generateHTML = (answers) =>
+
+;
+
+function init() {
+  console.log('hi');
+  try {
+    const answers = await promptUser();
+
+    const html = generateHTML(answers);
+
+    await writeFileAsync('index.html', html);
+
+    console.log('Successfully wrote to index.html');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
